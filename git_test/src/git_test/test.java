106a4f69e2e97f55538f00a6b3ec5800f26ffa40
TJOEUN <TJOEUN@DESKTOP-MJTQBRJ>
2021-06-15 15:20:46 +0900

    수정 박원형

--- git_test/src/git_test/test.java
+++ git_test/src/git_test/test.java
@@ -3,7 +3,7 @@ package git_test;
 public class test {
 	public static void main(String[] args) {
 		System.out.println("팀");
-		System.out.println("안녕하세요 강현우입니다");
+		System.out.println("안녕하세요 박원형입니다");
 		System.out.println("확인!");
 	}
 }
