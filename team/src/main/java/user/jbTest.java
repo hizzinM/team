4d3969b09241e484215858b723c54c4ed9e286c8
wwhhp <wwhhp@118.216.141.216>
2021-06-22 18:32:57 +0900

    원형

---
+++ team/src/main/java/user/jbTest.java
@@ -0,0 +1,28 @@
+package user;
+
+import static org.junit.Assert.fail;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.junit.Test;
+
+public class jbTest {
+	static {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void test() {
+		try (Connection con = DriverManager
+				.getConnection("jdbc:mysql://localhost:3306/shopping?serverTimezone=Asia/Seoul", "root", "mysql")) {
+			System.out.println(con);
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
+}
