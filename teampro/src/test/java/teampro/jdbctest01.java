ee27d7f1a839eb9d4d6116da2ed31604b4884ba7
hanueilw <pmy3581040@naver.com>
2021-06-22 16:41:35 +0900

    jdbc 수정

---
+++ teampro/src/test/java/teampro/jdbctest01.java
@@ -0,0 +1,28 @@
+package teampro;
+
+import static org.junit.Assert.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.junit.Test;
+
+public class jdbctest01 {
+	static {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void test() {
+		try (Connection con = DriverManager
+				.getConnection("jdbc:mysql://localhost:3306/shopping?serverTimezone=Asia/Seoul", "root", "mysql")) {
+			System.out.println(con);
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
+}
