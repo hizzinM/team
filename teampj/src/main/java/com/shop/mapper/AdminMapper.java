75d8fc56aa997ba1fcaee54964b6b8bf9c946c91
wwhhp <wwhhp@118.216.141.216>
2021-06-30 18:24:46 +0900

    상품목록-이미지 X

--- teampj/src/main/java/com/shop/mapper/AdminMapper.java
+++ teampj/src/main/java/com/shop/mapper/AdminMapper.java
@@ -1,5 +1,7 @@
 package com.shop.mapper;
 
+import java.util.List;
+
 import com.shop.model.AttachImageVO;
 import com.shop.model.Product;
 
@@ -9,5 +11,8 @@ public interface AdminMapper {
 	public void insertpro(Product product);
 	/* 이미지 등록 */
 	public void imageEnroll(AttachImageVO vo);
-
+	/* 상품 목록 */
+	public List<Product> selectproductList();
+	/*이미지 목록*/
+	public List<AttachImageVO> selectimgList();
 }
