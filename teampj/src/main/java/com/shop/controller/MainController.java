5a4464e9920493b10715344cb951b16fd144ea9d
hanueilw <pmy3581040@naver.com>
2021-07-06 13:11:32 +0900

    카테고리 페이지

--- teampj/src/main/java/com/shop/controller/MainController.java
+++ teampj/src/main/java/com/shop/controller/MainController.java
@@ -1,5 +1,5 @@
 package com.shop.controller;
- 
+
 import java.io.File;
 import java.io.IOException;
 
@@ -27,25 +27,68 @@ public class MainController {
 		logger.info("메인 페이지 진입");
 
 	}
+
 	@GetMapping("/display")
-	public ResponseEntity<byte[]> getImage(String fileName){
-		logger.info("getImage()........"+ fileName);
+	public ResponseEntity<byte[]> getImage(String fileName) {
+		logger.info("getImage()........" + fileName);
 		File file = new File("C:\\git\\team\\teampj\\src\\main\\webapp\\resources\\upload\\" + fileName);
-		
+
 		ResponseEntity<byte[]> result = null;
-		
+
 		try {
-			
+
 			HttpHeaders header = new HttpHeaders();
-			
+
 			header.add("Content-type", Files.probeContentType(file.toPath()));
-			
+
 			result = new ResponseEntity<>(FileCopyUtils.copyToByteArray(file), header, HttpStatus.OK);
-			
-		}catch (IOException e) {
+
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		return result;
+	}
+
+	// 아우터 페이지 이동
+	@RequestMapping(value = "/category/Outer", method = RequestMethod.GET)
+	public void getOuter() throws Exception {
+		logger.info("아우터 페이지 접속");
+	}
+
+	// 탑 페이지 이동
+	@RequestMapping(value = "/category/Top", method = RequestMethod.GET)
+	public void getTop() throws Exception {
+		logger.info("탑 페이지 접속");
+	}
+
+	// 셔츠 페이지 이동
+	@RequestMapping(value = "/category/Shirts", method = RequestMethod.GET)
+	public void getShirts() throws Exception {
+		logger.info("셔츠 페이지 접속");
+	}
+
+	// 팬츠 페이지 이동
+	@RequestMapping(value = "/category/Pants", method = RequestMethod.GET)
+	public void getPants() throws Exception {
+		logger.info("팬츠 페이지 접속");
+	}
+
+	// 수트 페이지 이동
+	@RequestMapping(value = "/category/Suit", method = RequestMethod.GET)
+	public void getSuit() throws Exception {
+		logger.info("수트 페이지 접속");
+	}
+
+	// 백/슈즈 페이지 이동
+	@RequestMapping(value = "/category/BagShoes", method = RequestMethod.GET)
+	public void getBagShoes() throws Exception {
+		logger.info("백/슈즈 페이지 접속");
+	}
+
+	// 악세사리 페이지 이동
+	@RequestMapping(value = "/category/Acc", method = RequestMethod.GET)
+	public void getAcc() throws Exception {
+		logger.info("악세사리 페이지 접속");
 	}
 }
